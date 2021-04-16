d45d1a2296d35794ee08464b85a93b54ad46ac4a
Vinícius Lins <viniciusaclins@gmail.com>
2021-04-16 17:22:43 -0300

    Alterando métodos da raiz

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,11 @@ const router = express.Router();
 const auth = require('../middlewares/auth')
 
 router.get('/', auth, (req,res) => {
-    console.log(res.locals.auth_data);
-    return res.send({message: "Essa informação é muito importante!"});
+    return res.json({message: "Método GET da raíz!"});
 })
 
 router.post('/', (req,res) => {
-    return res.send({message: "Tudo ok com o Método POST da raíz!"});
+    return res.json({message: "Tudo ok com o Método POST da raíz!"});
 })
 
 
